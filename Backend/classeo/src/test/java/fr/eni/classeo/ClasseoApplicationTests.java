a8f356ba1401c37697edb88da841b19242557dbd
Belovitchy <belo38690@gmail.com>
2026-08-11 09:17:25 +0200

    init spring boot

---
+++ Backend/classeo/src/test/java/fr/eni/classeo/ClasseoApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.classeo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasseoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
