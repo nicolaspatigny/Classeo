fbbc868ff03633508c1f20e0fd211653dce98a63
unknown <nickim.patigny@gmail.com>
2026-08-13 18:13:29 +0200

    merge avec branche userAuth

--- Backend/classeo/src/main/java/fr/eni/classeo/controller/AuthController.java
+++ Backend/classeo/src/main/java/fr/eni/classeo/controller/AuthController.java
@@ -1,19 +1,25 @@
 package fr.eni.classeo.controller;
 
 import fr.eni.classeo.dto.LoginRequest;
+import fr.eni.classeo.security.JwtService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
     private final AuthenticationManager authenticationManager;
+    private final JwtService jwtService;
 
-    public AuthController(AuthenticationManager authenticationManager) {
+    public AuthController(
+            AuthenticationManager authenticationManager,
+            JwtService jwtService) {
         this.authenticationManager = authenticationManager;
+        this.jwtService = jwtService;
     }
 
     @PostMapping("/login")
@@ -27,6 +33,8 @@ public class AuthController {
                         )
                 );
 
-        return "Authentication successful";
+        return jwtService.generateToken(
+                (UserDetails) authentication.getPrincipal()
+        );
     }
 }
---
+++ Backend/classeo/src/main/java/fr/eni/classeo/security/JwtService.java
@@ -0,0 +1,26 @@
+package fr.eni.classeo.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private final SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    public String generateToken(UserDetails userDetails) {
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 60))
+                .signWith(secretKey)
+                .compact();
+    }
+}
